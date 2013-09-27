676c8b03d1378e582e7922315a30bba69dff3fef
Leo.debian <changtong1993@gmail.com>
2013-09-28 03:11:32 +0800

    data.py

---
+++ blog/admin.py
@@ -0,0 +1,7 @@
+#coding=utf8
+
+from flask.ext.admin import Admin
+from flask.ext.admin.contrib.sqlamodel import ModelView
+from blog import app
+from data import (db, Category, Tag, Article, Comment, User,\
+        Link, BlackList, Subscriber)
