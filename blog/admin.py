2053689b7b477a6e8406e4c81e676b0a1725d8ed
Leo.debian <changtong1993@gmail.com>
2013-10-18 19:42:01 +0800

    admin.py complete.

--- blog/admin.py
+++ blog/admin.py
@@ -4,3 +4,18 @@ from flask.ext.admin.contrib.sqlamodel import ModelView
 from blog import app
 from data import (db, Category, Tag, Article, Comment, User,\
         Link, BlackList, Subscriber)
+
+class MyModelView(ModelView):
+    def is_accessible(self):
+        return True
+
+admin = Admin(app, url='blog/admin')
+
+admin.add_view(MyModelView(Category, db.session))
+admin.add_view(MyModelView(Tag, db.session))
+admin.add_view(MyModelView(Article, db.session))
+admin.add_view(MyModelView(Comment, db.session))
+admin.add_view(MyModelView(User, db.session))
+admin.add_view(MyModelView(Link, db.session))
+admin.add_view(MyModelView(BlackList, db.session))
+admin.add_view(MyModelView(Subscriber, db.session))
