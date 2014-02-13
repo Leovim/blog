dbf09adf66953f2d8e99b34e9efd80d16c8a8a3c
Leo Liu <changtong1993@gmail.com>
2014-02-13 14:38:13 +0800

    debug on

--- myblog.py
+++ myblog.py
@@ -4,7 +4,8 @@ from flask import Flask
 #from blog.database import db_session
 
 app = Flask(__name__)
-app.config.from_object('config')
+#app.config.from_object('config')
+app.debug = True
 
 @app.route('/')
 def hello_world():
@@ -19,4 +20,4 @@ def test():
     return '正在測試.'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
